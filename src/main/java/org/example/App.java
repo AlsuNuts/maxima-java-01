9d8c1c0207d162a4235904d884b0b1901d57da7f
AlsuNuts <forever-nuts@yandex.ru>
2022-09-09 20:48:18 +0300

    Init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(daysCount((byte) 9, 2000));
+        System.out.println(daysCount((byte) 2, 2001));
     }
 
     public static String daysCount(byte month, int year) {
@@ -25,8 +28,9 @@ public class App {
                 result = "30";
                 break;
             case 2:
-                return year % 4 == 0 & (year % 100 != 0 | year % 400 == 0) ? "29" : "28";
+                result = year % 4 == 0 & (year % 100 != 0 | year % 400 == 0) ? "29" : "28";
         }
         return result;
     }
 }
+
