45b2fc6f8ab38c488cc1ca27eefea7aafdb7ba6e
AlsuNuts <forever-nuts@yandex.ru>
2022-09-09 17:26:22 +0300

    Init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,14 @@
 package org.example;
 
 
-//Написать функцию byte daysCount(byte month, int year), которая по номеру месяца и номеру года возвращает число дней в месяце.
-// Год високосный, если year % 4 == 0 & (year % 100 != 0 | year % 400 == 0
-// 31 день - 1, 3, 5, 7, 8, 10, 12
-// 30 дней - 4, 6, 9, 11
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println(daysCount((byte) 2, 2004 ));
+public class App {
+    public static void main(String[] args) {
+        System.out.println(daysCount((byte) 9, 2000));
     }
-    public static String daysCount(byte month, int year)
-    {
 
+    public static String daysCount(byte month, int year) {
         String result = "";
-        switch (month){
+        switch (month) {
             case 1:
             case 3:
             case 5:
@@ -23,7 +16,7 @@ public class App
             case 8:
             case 10:
             case 12:
-                result  = "31";
+                result = "31";
                 break;
             case 4:
             case 6:
@@ -31,26 +24,9 @@ public class App
             case 11:
                 result = "30";
                 break;
-                // если не получится сделать иф внутри снича, то попробовать февраль сделать отдельно
             case 2:
-                if (year % 4 == 0 & (year % 100 != 0 | year % 400 == 0)) {
-                    return result = "29";
-                }
-                result = "28";
-
-            }
-
+                return year % 4 == 0 & (year % 100 != 0 | year % 400 == 0) ? "29" : "28";
+        }
         return result;
     }
 }
-    /* public static long fibonazzi (byte N)
-    {
-        return N <= 1 ? N : fibonazzi((byte) (N-1)) + fibonazzi((byte)(N-2));
-    }
-
-    if (month == 2) {
-            if (year % 4 == 0 & (year % 100 != 0 | year % 400 == 0)) {
-                result = "29";
-            }
-        }
-            result = "28"; */
